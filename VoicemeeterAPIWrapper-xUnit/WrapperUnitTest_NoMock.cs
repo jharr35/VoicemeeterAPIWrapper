fec4236db25c1cc20b89da9656b7e5f65fe8a13a
James Harris <jharr35@wgu.edu>
2023-12-06 00:24:17 -0800

    Test Updates

---
+++ VoicemeeterAPIWrapper-xUnit/WrapperUnitTest_NoMock.cs
@@ -0,0 +1,31 @@
+﻿using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoicemeeterAPIWrapperLibrary;
+
+namespace VoicemeeterAPIWrapper_xUnit
+{
+    public class WrapperUnitTest_NoMock
+    {
+        private readonly VoicemeeterAPIWrapper _voicemeeter;
+
+        public WrapperUnitTest_NoMock() 
+        {
+            var loggerMock = new Mock<ILogger<VoicemeeterAPIWrapper>>();
+            _voicemeeter = new VoicemeeterAPIWrapper(loggerMock.Object);
+        }
+
+        [Fact]
+        public void GetParameterFloat_ReturnGainOfA1()
+        {
+            var result = _voicemeeter.GetParameterFloat("Strip[0].A1");
+
+            //Assert
+            Assert.Equal(0.0f, result);
+        }
+    }
+}
--- VoicemeeterAPIWrapper-xUnit/obj/Debug/net7.0/VoicemeeterAPIWrapper-xUnit.AssemblyInfo.cs
+++ VoicemeeterAPIWrapper-xUnit/obj/Debug/net7.0/VoicemeeterAPIWrapper-xUnit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoicemeeterAPIWrapper-xUnit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc318a4f3a4b492d7bc7a235f2835b3a375743c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoicemeeterAPIWrapper-xUnit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoicemeeterAPIWrapper-xUnit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
